9655372d17049994fdcd28ac7c671aab04814d25
almarpa <alex_marco7@outlook.es>
2019-09-17 13:44:50 +0200

    Adding a map with Leaflet

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@
     var regalo = document.getElementById('regalo');
     document.addEventListener('DOMContentLoaded', function(){
     
+        var map = L.map('mapa').setView([39.467554, -0.375732], 14);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        
+        L.marker([39.467554, -0.375732]).addTo(map)
+            .bindTooltip('VLCWebCamp')
+            .openTooltip();
+
+
         // Campos datos de usuario
         var nombre = document.getElementById('nombre');
         var apellido = document.getElementById('apellido');
