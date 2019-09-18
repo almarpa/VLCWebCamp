951c44a45bca84c72807bf2b0b6d97e9c8726717
almarpa <alex_marco7@outlook.es>
2019-09-18 19:02:06 +0200

    List of events section implemented with JQuery. Possibility of choosing between conferences, seminars and workshops.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+//JAVASCRIPT
 (function() {
     "use strict";
 
     document.addEventListener('DOMContentLoaded', function(){
+        
+        // Section Map
         var map = L.map('mapa').setView([39.467554, -0.375732], 14);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -13,3 +16,22 @@
             .openTooltip();
     }); 
 })();
+
+// JQUERY
+$(function() {
+    // Programa de Conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+
+    $('.menu-programa a').on('click', function() {
+        $('.ocultar').hide();
+        
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+
+        return false;
+    });
+});
