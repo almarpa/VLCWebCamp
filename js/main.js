881d955b413eedf452806989969947b810dcb262
almarpa <alex_marco7@outlook.es>
2019-09-20 23:57:07 +0200

    JQuery pluggins added with their respective files and references. Section 'Conferences' created.

--- js/main.js
+++ js/main.js
@@ -16,9 +16,10 @@
             .openTooltip();
     }); 
 })();
-
+ 
 // JQUERY
 $(function() {
+
     // Programa de Conferencias
     $('.programa-evento .info-curso:first').show();
     $('.menu-programa a:first').addClass('activo');
@@ -34,4 +35,19 @@ $(function() {
 
         return false;
     });
+
+    // Animaciones para los números
+    $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6}, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15}, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3}, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9}, 1500);
+
+    // Animacion para el contador
+    $('.cuenta-regresiva').countdown('2019/12/10 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
 });
