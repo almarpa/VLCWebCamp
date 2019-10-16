23d56c01aa3cc04dde48c21a03c008724fdf449b
almarpa <alex_marco7@outlook.es>
2019-10-16 11:44:21 +0200

    Error showing style with css solved

--- calendario.php
+++ calendario.php
@@ -1,6 +1,6 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-<section class="galeria-fotos contenedor">
+<section class="seccion contenedor">
     <h2>Calendario de eventos</h2>
 
     <?php
@@ -19,65 +19,68 @@
     }
     ?>
 
-</section>
+    <div class="calendario">
+        <?php
+            $calendario = array();
+            while($eventos = $resultado->fetch_assoc()) { 
 
-<div class="calendario">
-    <?php
-        $calendario = array();
-        while($eventos = $resultado->fetch_assoc()) { 
+                //Obtiene la fecha del evento
+                $fecha = $eventos['fecha_evento'];
 
-            //Obtiene la fecha del evento
-            $fecha = $eventos['fecha_evento'];
-
-            $evento = array(
-                'titulo' => $eventos['nombre_evento'],
-                'fecha' => $eventos['fecha_evento'],
-                'hora' => $eventos['hora_evento'],
-                'categoria' => $eventos['cat_evento'],
-                'icono' => 'fa' . " " . $eventos['icono'],
-                'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
-            );
-            
-            //Manera de agrupar por fecha
-            $calendario[$fecha][] = $evento;
-
-        } ?>
-
-
-    <?php
-        foreach($calendario as $dia => $lista_eventos) { ?>
-            <h3>
-                <i class="fa fa-calendar"></i>
-                <?php 
-                    setLocale(LC_TIME,'spanish');
+                $evento = array(
+                    'titulo' => $eventos['nombre_evento'],
+                    'fecha' => $eventos['fecha_evento'],
+                    'hora' => $eventos['hora_evento'],
+                    'categoria' => $eventos['cat_evento'],
+                    'icono' => $eventos['icono'],
+                    'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
+                );
                 
-                    echo strftime( "%A, %d de %B del %Y", strtotime($dia) ); 
-                ?>
-            </h3>
-            <?php foreach($lista_eventos as $evento) { ?>
-                <div class="dia">
-                    <p class="titulo">
-                        <?php echo $evento['titulo']; ?>
-                    </p>
-                    <p class="hora">
-                        <i class="fa fa-clock" aria-hidden="true"></i>
-                        <?php echo $evento['fecha'] . " " . $evento['hora']; ?>
-                    </p>
-                    <p>
-                        <i class="fa <?php echo $evento['icono']; ?>" aria-hidden="true"></i>
-                        <?php echo $evento['categoria']; ?></p>
-                    </p>
-                    <p>
-                        <i class="fa fa-user" aria-hidden="true"></i>
-                        <?php echo $evento['invitado']; ?></p>
-                    </p>
-                </div>
-            <?php } //Fin foreach eventos ?> 
-        <?php } //Fin foreach dias?>
-</div>
+                //Manera de agrupar por fecha
+                $calendario[$fecha][] = $evento;
 
-<?php
-    $conn->close();
-?>
+                ?>
+            <?php } // while de fetch_assoc?>
+
+
+        <?php
+            foreach($calendario as $dia => $lista_eventos) { ?>
+                <h3>
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                    <?php 
+                        setLocale(LC_TIME,'spanish');
+                    
+                        echo strftime( "%A, %d de %B del %Y", strtotime($dia) ); 
+                    ?>
+                    
+                </h3>
+                <?php foreach($lista_eventos as $evento) { ?>
+                    <div class="dia">
+                        <p class="titulo">
+                            <?php echo $evento['titulo']; ?>
+                        </p>
+                        <p class="hora">
+                            <i class="fa fa-clock" aria-hidden="true"></i>
+                            <?php echo $evento['fecha'] . " " . $evento['hora']; ?>
+                        </p>
+                        <p>
+                            <i class="fa <?php echo $evento['icono']; ?>" aria-hidden="true"></i>
+                            <?php echo $evento['categoria']; ?></p>
+                        </p>
+                        <p>
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <?php echo $evento['invitado']; ?></p>
+                        </p>
+                        
+                    </div>
+                <?php } //Fin foreach eventos ?> 
+            <?php } //Fin foreach dias ?>
+    </div>
+
+    <?php
+        $conn->close();
+    ?>
+
+</section>
 
 <?php include_once 'includes/templates/footer.php'; ?>
